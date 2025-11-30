409a1fbe6cdbd3184bdf1eabc1237cf19a154024
Hugo García Álvarez <hgarciaa3@alumnos.nebrija.es>
2025-11-30 14:02:08 +0100

    Delete Angular directory

--- Angular/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { AppModule } from './app.module';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    // Importa los providers declarados en AppModule (incluye FormsModule / RouterModule)
-    importProvidersFrom(AppModule)
-  ]
-};
